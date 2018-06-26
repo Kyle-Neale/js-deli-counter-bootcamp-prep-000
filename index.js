806c7e96390d647ef6af661418fd3afd82ade487
Kyle Neale <kyle.a.neale@gmail.com>
2018-06-26 18:14:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var katzDeli = [];
 
-function takeANumber(katzDeli, name) {
-  katzDeli.push(name);
-  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing(line){
+function nowServing(line) {
   if(!line.length) {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   } else {
     return "Currently serving " + line.shift() + ".";
   }
@@ -17,12 +17,12 @@ function currentLine(line) {
   var lineNameAndNumbers = [];
   
   if (!line.length) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
   for (var i = 0; i < line.length; i++) {
     var name = line[i];
     
     lineNameAndNumbers.push((i + 1) + '. ' + name);
   }
-  return "The line is currently: " + lineNameAndNumbers.join(', ')
+  return "The line is currently: " + lineNameAndNumbers.join(', ');
 }
